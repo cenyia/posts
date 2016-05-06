9a96f0222633fe461bd1985f6ec36f70b0894bd1
cenyia <chidienyia@hotmail.com>
2016-05-06 02:53:26 +0000

    Initial commit

--- posts/database.py
+++ posts/database.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -8,3 +9,19 @@ engine = create_engine(app.config["DATABASE_URI"])
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
+
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(128))
+    body = Column(String(1024))
+    
+    def as_dictionary(self):
+        post = {
+            "id": self.id,
+            "title": self.title,
+            "body": self.body
+        }
+        return post
