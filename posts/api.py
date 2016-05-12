ab71174cae99f0e2cf0cfccb1bc024206b9c9ec4
cenyia <chidienyia@hotmail.com>
2016-05-12 20:27:47 +0000

    second commit

--- posts/api.py
+++ posts/api.py
@@ -1,3 +1,12 @@
+# JSON Schema describing the structure of a post
+post_schema = {
+    "properties": {
+        "title" : {"type" : "string"},
+        "body": {"type": "string"}
+    },
+    "required": ["title", "body"]
+}
+
 import json
 
 from flask import request, Response, url_for
@@ -6,16 +15,27 @@ from jsonschema import validate, ValidationError
 from . import models
 from . import decorators
 from posts import app
-from .database import session
+from .database import session, Post
 
+from . import database
 
 @app.route("/api/posts", methods=["GET"])
 @decorators.accept("application/json")
 def posts_get():
     """ Get a list of posts """
 
+    # Get the querystring arguments
+    title_like = request.args.get("title_like")
+
     # Get the posts from the database
-    posts = session.query(models.Post).order_by(models.Post.id)
+    #posts = session.query(Post).order_by(Post.id)
+    
+    # Get and filter the posts from the database
+    posts = session.query(database.Post)
+    if title_like:
+        posts = posts.filter(database.Post.title.contains(title_like))
+    posts = posts.order_by(database.Post.id)
+    
 
     # Convert the posts to JSON and return a response
     data = json.dumps([post.as_dictionary() for post in posts])
@@ -27,7 +47,7 @@ def posts_get():
 def post_get(id):
     """ Single post endpoint """
     # Get the post from the database
-    post = session.query(models.Post).get(id)
+    post = session.query(Post).get(id)
 
     # Check whether the post exists
     # If not return a 404 with a helpful message
@@ -39,3 +59,31 @@ def post_get(id):
     # Return the post as JSON
     data = json.dumps(post.as_dictionary())
     return Response(data, 200, mimetype="application/json")
+    
+
+@app.route("/api/posts", methods=["POST"])
+@decorators.accept("application/json")
+@decorators.require("application/json")
+def posts_post():
+    """ Add a new post """
+    data = request.json
+
+    # Check that the JSON supplied is valid
+    # If not you return a 422 Unprocessable Entity
+    try:
+        validate(data, post_schema)
+    except ValidationError as error:
+        data = {"message": error.message}
+        return Response(json.dumps(data), 422, mimetype="application/json")
+
+    # Add the post to the database
+    post = database.Post(title=data["title"], body=data["body"])
+    session.add(post)
+    session.commit()
+
+    # Return a 201 Created, containing the post as JSON and with the
+    # Location header set to the location of the post
+    data = json.dumps(post.as_dictionary())
+    headers = {"Location": url_for("post_get", id=post.id)}
+    return Response(data, 201, headers=headers,
+                    mimetype="application/json")
